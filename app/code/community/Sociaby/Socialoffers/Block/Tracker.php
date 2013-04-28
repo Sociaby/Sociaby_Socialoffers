9f3321f72b295599f086a0589d875dd6d47cd26d
Seth Malaki <seth.malaki@gmail.com>
2013-04-28 19:30:53 +0800

    Forgot to rename this shindig

--- app/code/community/Sociaby/Socialoffers/Block/Tracker.php
+++ app/code/community/Sociaby/Socialoffers/Block/Tracker.php
@@ -1,6 +1,6 @@
 <?php
 
-class Sociaby_Socialoffers_Block_Offers extends Mage_Core_Block_Template {
+class Sociaby_Socialoffers_Block_Tracker extends Mage_Core_Block_Template {
     public function getSociabyId() {
         return Mage::getStoreConfig('offers/general/unique_id');
     }

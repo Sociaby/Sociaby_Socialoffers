1c5e960f0c41894eac404b1285c179cf2287ffa4
Seth Malaki <seth.malaki@gmail.com>
2013-04-28 17:49:35 +0800

    Correction to tests

--- tests/integration/admin.js
+++ tests/integration/admin.js
@@ -1,25 +1,26 @@
 var casper = require('casper').create();
+var adminKey;
 
+casper
+    .start('http://magen.to/admin', function() {
+        this.test.assertTitle('Log into Magento Admin Page', 'Can access admin login page normally.');
+        this.test.assertExists('form#loginForm', 'main login form is found');
+        this.fill('form#loginForm', {
+            'login[username]' : 'admin',
+    	'login[password]' : 'admin123'
+        }, true);
+    })
 
-casper.start('http://magen.to/admin', function() {
-    this.test.assertTitle('Log into Magento Admin Page', 'Can access admin login page normally.');
-    this.test.assertExists('form#loginForm', 'main login form is found');
-    this.fill('form#loginForm', {
-        'login[username]' : 'admin',
-	'login[password]' : 'admin123'
-    }, true);
-});
+    .userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X)')
+    .viewport(1024, 768)
 
-casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X)');
-casper.viewport(1024, 768);
-
-
-casper.then(function() {
-    this.test.assertTitle('Dashboard / Magento Admin', 'Can login to magento admin properly');
-    this.test.assertExists('ul#nav', 'Navigation bar is shown.');
-});
-
-casper.run(function() {
-    //this.test.done(9); 
-    this.test.renderResults(true);
-});
+    .then(function() {
+        this.test.assertTitle('Dashboard / Magento Admin', 'Can login to magento admin properly');
+        this.test.assertExists('ul#nav', 'Navigation bar is shown.');
+        adminKey = this.getCurrentUrl().match(/([a-z0-9]+)\/$/i)[0];
+        this.echo("Current URL Key is : " + adminKey);
+    })
+    .run(function() {
+        //this.test.done(9); 
+        this.test.renderResults(true);
+    });

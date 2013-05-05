2cc34ca7919a312515b777b6ba57157122f9eaf4
Seth Malaki <seth.malaki@gmail.com>
2013-05-05 13:39:01 +0800

    Added render condition based on configuration settings

--- app/code/community/Sociaby/Socialoffers/Block/Tracker.php
+++ app/code/community/Sociaby/Socialoffers/Block/Tracker.php
@@ -4,4 +4,7 @@ class Sociaby_Socialoffers_Block_Tracker extends Mage_Core_Block_Template {
     public function getSociabyId() {
         return Mage::getStoreConfig('offers/general/unique_id');
     }
+    public function canRender() {
+	return Mage::getStoreConfig('offers/general/conversion_tracking') === 1;
+    }
 }

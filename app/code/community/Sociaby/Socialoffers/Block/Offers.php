04646a88c0336b0b17910fcd46eb177e87e0cb60
Seth Malaki <seth.malaki@gmail.com>
2013-07-20 20:37:04 +0800

    Changed product URL routine to use parent's URL if it's a child

--- app/code/community/Sociaby/Socialoffers/Block/Offers.php
+++ app/code/community/Sociaby/Socialoffers/Block/Offers.php
@@ -17,8 +17,22 @@ class Sociaby_Socialoffers_Block_Offers extends Mage_Core_Block_Template{
 
 		
 		foreach ($cartItems as $item) {
+
+			$parentIds = Mage::getModel('catalog/product_type_configurable')
+							->getParentIdsByChild(
+									$item
+										->getProduct()
+										->getId()
+							);
+
 		    array_push($sociabyProductsObjectArray['items'], $item->getName());
-		    array_push($sociabyProductsObjectArray['urls'], $item->getProduct()->getProductUrl());
+		    array_push($sociabyProductsObjectArray['urls'], 
+
+		    	(empty($parentIds)) ?
+		    		$item->getProduct()->getProductUrl() :
+		    		Mage::getModel('catalog/product')->load(is_array($parentIds) ? $parentIds[0] : $parentIds)->getProductUrl()
+		    );
+		    
 		    $image = (string) $this->helper('catalog/image')->init($item->getProduct(), 'thumbnail')->resize(150, 150);
 		    array_push($sociabyProductsObjectArray['images'], $image);
 		    array_push($sociabyProductsObjectArray['prices'], round($item->getPrice(),2));
